2e385e2d986eebbc4e3075aa5ffeb82ee7ef2b4a
nivmaman <nivvm123@gmail.com>
2018-01-21 10:02:09 +0200

    project init.

---
+++ app/src/main/java/com/israir/nik/tazminlitisa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.israir.nik.tazminlitisa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
